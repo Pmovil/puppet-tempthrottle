d1bc7856bdc338c5914a1ce368512d98a07839a8
f_cabeca <fabricio@pmovil.com.br>
2015-12-07 19:57:08 -0200

    first version

---
+++ README.md
@@ -0,0 +1,50 @@
+# tempthrottle
+
+#### Table of Contents
+
+1. [Overview](#overview)
+2. [Module Description - What the module does and why it is useful](#module-description)
+3. [Setup - The basics of getting started with tempthrottle](#setup)
+    * [What tempthrottle affects](#what-tempthrottle-affects)
+    * [Setup requirements](#setup-requirements)
+    * [Beginning with tempthrottle](#beginning-with-tempthrottle)
+4. [Usage - Configuration options and additional functionality](#usage)
+5. [Reference - An under-the-hood peek at what the module is doing and how](#reference)
+5. [Limitations - OS compatibility, etc.](#limitations)
+6. [Development - Guide for contributing to the module](#development)
+
+## Overview
+
+Controls the cpu frequency in order to avoid overheating.
+
+## Module Description
+
+Under the hood it uses a fork from https://github.com/Sepero/temp-throttle
+
+## Setup
+
+### What tempthrottle affects
+
+
+### Setup Requirements **OPTIONAL**
+
+
+### Beginning with tempthrottle
+
+
+## Usage
+
+
+## Reference
+
+
+## Limitations
+
+Tested on CentOS 7, requires SystemD to work.
+
+## Development
+
+
+## Release Notes/Contributors/Etc **Optional**
+
+Thanks to Sepero for the temp-throttle script
---
+++ manifests/init.pp
@@ -0,0 +1,74 @@
+# == Class: tempthrottle
+#
+# Full description of class tempthrottle here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the function of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { 'tempthrottle':
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2015 Your name here, unless otherwise noted.
+#
+class tempthrottle {
+  # main script
+  file { '/usr/sbin/temp-throttle':
+            owner => root,
+            group => root,
+            mode  => '0755',
+            ensure => present,
+            source => "puppet:///tempthrottle/temp-throttle/usr/sbin/temp-throttle",
+  }
+  # config file
+	file { '/etc/temp-throttle.conf':
+	          owner => root,
+            group => root,
+            mode  => '0644',
+            ensure => present,
+	          source => "files/temp-throttle/etc/temp-throttle.conf",
+  }
+  
+  if($::operatingsystem == 'CentOS'){
+    # systemd service
+	  file { '/usr/lib/systemd/system/temp-throttle.service':
+	            owner => root,
+	            group => root,
+	            mode  => '0644',
+	            ensure => present,
+	            source => "files/temp-throttle/usr/lib/systemd/system/temp-throttle.service",
+	  }
+	  
+	  service { 'temp-throttle':
+	    ensure     => running,
+	    enable     => true,
+	    hasstatus  => true,
+	    hasrestart => true,
+	    require    => File['/usr/lib/systemd/system/temp-throttle.service'],
+	  }
+  } # TODO - fedora 14 SysV script ?
+}
